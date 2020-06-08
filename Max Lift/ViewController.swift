386117f9ed217db302eec9357936d051fa5fead6
colperei <colperei@ucsc.edu>
2020-06-07 21:55:41 -0700

    Initial

---
+++ Max Lift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Max Lift
+//
+//  Created by Conrad Pereira on 6/7/20.
+//  Copyright © 2020 Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

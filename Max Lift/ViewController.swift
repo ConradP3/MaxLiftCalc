2dba4f834d478625e82856d9b1f159c002725a00
colperei <colperei@ucsc.edu>
2020-06-11 16:41:26 -0700

    simple calculation

--- Max Lift/ViewController.swift
+++ Max Lift/ViewController.swift
@@ -9,12 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var weightLb: UITextField!
+    
+    @IBOutlet weak var maxReps: UITextField!
+    
+    @IBOutlet weak var out: UILabel!
+    
+    @IBOutlet weak var compLift: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
+       
     }
-
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+ 
+   
+    
+    @IBAction func calcMax(_ sender: Any) {
+        
+        var liftType = compLift.text
+        let weightVal = Double(weightLb.text!)
+        let repVal = Double(maxReps.text!)
+        
+        
+        
+        let maxLift = Double(weightVal!+repVal!*5)
+        
+       
+        
+        
+        out.text = "Predicted Max: \(maxLift)"
+        
+    }
+    
+    func squat(weight: Double, reps: Double) -> Double {
+        let maxLift = Double(weight+reps*5)
+        
+        return Double(maxLift)
+        
+    }
+    
+    
 }
 

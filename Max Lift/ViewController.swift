2e49c6244ed78e40545eaa2768f53cef92c15bfa
colperei <colperei@ucsc.edu>
2020-07-03 20:58:18 -0700

    Multiple lifts

--- Max Lift/ViewController.swift
+++ Max Lift/ViewController.swift
@@ -33,23 +33,69 @@ class ViewController: UIViewController {
     
     @IBAction func calcMax(_ sender: Any) {
         
-        var liftType = compLift.text
+        let liftType = compLift.text
+        var maxLift = Double(0.0)
         let weightVal = Double(weightLb.text!)
         let repVal = Double(maxReps.text!)
         
+        if(liftType! == "squat"){
+            maxLift = squat(weight: weightVal!, reps: repVal!)
+            out.text = "Predicted Max: \(maxLift)"
+            
+        }else if(liftType! == "bench"){
+            maxLift = bench(weight: weightVal!, reps: repVal!)
+            out.text = "Predicted Max: \(maxLift)"
+            
+        }else if(liftType! == "deadlift"){
+            maxLift = deadlift(weight: weightVal!, reps: repVal!)
+            out.text = "Predicted Max: \(maxLift)"
+            
+        }else if(liftType! == "hangclean"){
+            maxLift = hangclean(weight: weightVal!, reps: repVal!)
+            out.text = "Predicted Max: \(maxLift)"
+            
+        }else{
+            out.text = "Error Enter: squat, bench, deadlift, or handclean"
+        }
+    
         
         
-        let maxLift = Double(weightVal!+repVal!*5)
         
-       
-        
-        
-        out.text = "Predicted Max: \(maxLift)"
         
     }
     
     func squat(weight: Double, reps: Double) -> Double {
-        let maxLift = Double(weight+reps*5)
+        
+        let k = Double(0.0333)
+        // Brzycki formula
+        let maxLift = Double((weight*reps*(k))+weight)
+        
+        return Double(maxLift)
+        
+    }
+    func bench(weight: Double, reps: Double) -> Double {
+        
+        let k = Double(0.0333)
+        // Brzycki formula
+        let maxLift = Double((weight*reps*(k))+weight)
+        
+        return Double(maxLift)
+        
+    }
+    func deadlift(weight: Double, reps: Double) -> Double {
+        
+        let k = Double(0.0333)
+        // Brzycki formula
+        let maxLift = Double((weight*reps*(k))+weight)
+        
+        return Double(maxLift)
+        
+    }
+    func hangclean(weight: Double, reps: Double) -> Double {
+        
+        let k = Double(0.0333)
+        // Brzycki formula
+        let maxLift = Double((weight*reps*(k))+weight)
         
         return Double(maxLift)
         
